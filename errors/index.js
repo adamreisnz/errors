372e3b70cda7a590ee72d7d9a518515101219f67
Adam Reis <adam@reis.nz>
2019-02-22 22:08:18 +1300

    Update index.js

--- errors/index.js
+++ errors/index.js
@@ -27,6 +27,7 @@ exports.FileTooLargeError = require('./client/file-too-large');
 exports.InvalidDataError = require('./client/invalid-data');
 exports.InvalidTokenError = require('./client/invalid-token');
 exports.NotFoundError = require('./client/not-found');
+exports.TooManyRequestsError = require('./client/too-many-requests');
 exports.ValidationError = require('./client/validation');
 
 /**
